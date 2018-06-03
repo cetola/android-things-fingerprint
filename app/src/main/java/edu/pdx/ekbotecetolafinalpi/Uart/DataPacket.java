96b4f9a4ec51ee5fed27c856fe961b55e29b47ac
Stephano Cetola <stephano.cetola@linux.intel.com>
2018-06-02 18:17:15 -0700

    abstract device info out into object
    Signed-off-by: Stephano Cetola <stephano.cetola@linux.intel.com>

--- app/src/main/java/edu/pdx/ekbotecetolafinalpi/Uart/DataPacket.java
+++ app/src/main/java/edu/pdx/ekbotecetolafinalpi/Uart/DataPacket.java
@@ -2,16 +2,13 @@ package edu.pdx.ekbotecetolafinalpi.Uart;
 
 import android.util.Log;
 
+import edu.pdx.ekbotecetolafinalpi.account.DeviceInfo;
+
 public class DataPacket extends Message {
     private static final String TAG = "DataPacket";
     public static final int MOD_INFO_SIZE = 30;
     private int size;
-    private final int VERSION_OFFSET = 8;
-    private final int ISO_SIZE_OFFSET = 16;
-    private final int DEVICE_SN_OFFSET = 24;
-    private String firmwareVersion;
-    private String isoMaxSize;
-    private String deviceSN;
+    private DeviceInfo deviceInfo;
 
     public DataPacket(int size) {
         super(size);
@@ -27,34 +24,36 @@ public class DataPacket extends Message {
     }
 
     public String getInfo() {
-        String info = null;
+        String rtn = null;
         switch (getSize()) {
             case MOD_INFO_SIZE:
-                processData();
-                info = "Firmware Version: " + firmwareVersion;
-                info += "\nIso Size Max: " + isoMaxSize;
-                info += "\nDevice SN: " + deviceSN;
+                rtn = getDeviceInfo().toString();
                 break;
             default:
                 Log.e(TAG, "getInfo: Data Size Unknown. Cannot get info.");
                 break;
         }
-        return info;
+        return rtn;
+    }
+
+    public DeviceInfo getDeviceInfo() {
+        if(deviceInfo == null) {
+            processData();
+        }
+        return deviceInfo;
     }
 
     private void processData() {
-        String d = UartUtils.bytesToHex(getData().array(), getData().array().length);
-        firmwareVersion = untangleFirmwareVersion(d.substring(VERSION_OFFSET, ISO_SIZE_OFFSET));
-        isoMaxSize = d.substring(ISO_SIZE_OFFSET, DEVICE_SN_OFFSET);
-        deviceSN = d.substring(DEVICE_SN_OFFSET);
-    }
-
-    private String untangleFirmwareVersion(String tangle) {
-        char[] chars = tangle.toCharArray();
-        return new StringBuilder().append(chars[6]).append(chars[7])
-                .append(chars[4]).append(chars[5])
-                .append(chars[2]).append(chars[3])
-                .append(chars[0]).append(chars[1])
-                .toString();
+        switch (getSize()) {
+            case MOD_INFO_SIZE:
+                deviceInfo = new DeviceInfo();
+                String d = UartUtils.bytesToHex(getData().array(), getData().array().length);
+                deviceInfo.setFirmwareVersion(d.substring(DeviceInfo.VERSION_OFFSET, DeviceInfo.ISO_SIZE_OFFSET));
+                deviceInfo.setIsoMaxSize(d.substring(DeviceInfo.ISO_SIZE_OFFSET, DeviceInfo.DEVICE_SN_OFFSET));
+                deviceInfo.setDeviceSN(d.substring(DeviceInfo.DEVICE_SN_OFFSET));
+                break;
+            default:
+                Log.e(TAG, "processData: Data Size Unknown. Cannot process data.");
+        }
     }
 }
---
+++ app/src/main/java/edu/pdx/ekbotecetolafinalpi/account/DeviceInfo.java
@@ -0,0 +1,49 @@
+package edu.pdx.ekbotecetolafinalpi.account;
+
+public class DeviceInfo {
+    private String firmwareVersion;
+    private String isoMaxSize;
+    private String deviceSN;
+
+    public static final int VERSION_OFFSET = 8;
+    public static final int ISO_SIZE_OFFSET = 16;
+    public static final int DEVICE_SN_OFFSET = 24;
+
+    public String getFirmwareVersion() {
+        return firmwareVersion;
+    }
+
+    public void setFirmwareVersion(String firmwareVersion) {
+        char[] chars = firmwareVersion.toCharArray();
+        this.firmwareVersion = new StringBuilder().append(chars[6]).append(chars[7])
+                .append(chars[4]).append(chars[5])
+                .append(chars[2]).append(chars[3])
+                .append(chars[0]).append(chars[1])
+                .toString();
+    }
+
+    public String getIsoMaxSize() {
+        return isoMaxSize;
+    }
+
+    public void setIsoMaxSize(String isoMaxSize) {
+        this.isoMaxSize = isoMaxSize;
+    }
+
+    public String getDeviceSN() {
+        return deviceSN;
+    }
+
+    public void setDeviceSN(String deviceSN) {
+        this.deviceSN = deviceSN;
+    }
+
+    @Override
+    public String toString() {
+        return "DeviceInfo{" +
+                "firmwareVersion='" + getFirmwareVersion() + '\'' +
+                ", isoMaxSize='" + getIsoMaxSize() + '\'' +
+                ", deviceSN='" + getDeviceSN() + '\'' +
+                '}';
+    }
+}
