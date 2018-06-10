dccd759e65fdd3337db54d48f2e3d7f66300941f
Stephano Cetola <stephano.cetola@linux.intel.com>
2018-06-10 09:14:33 -0700

    add realtime database example
    Signed-off-by: Stephano Cetola <stephano.cetola@linux.intel.com>

--- app/src/main/java/edu/pdx/ekbotecetolafinalpi/managers/ExampleManager.java
+++ app/src/main/java/edu/pdx/ekbotecetolafinalpi/managers/ExampleManager.java
@@ -1,8 +1,12 @@
 package edu.pdx.ekbotecetolafinalpi.managers;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 
@@ -11,18 +15,37 @@ import java.util.Random;
 import edu.pdx.ekbotecetolafinalpi.account.User;
 import edu.pdx.ekbotecetolafinalpi.dao.UserDao;
 import edu.pdx.ekbotecetolafinalpi.dao.UserDaoImpl;
+import edu.pdx.ekbotecetolafinalpi.realtime.RegisterFingerprint;
 
 public class ExampleManager {
 
     private static final String TAG = "ExampleManager";
+    private FirestoreManager dbManager;
     private UserDao userDao;
     private Random rand;
     private User mUser;
 
     public ExampleManager(FirestoreManager dbManager) {
         rand = new Random();
+        this.dbManager = dbManager;
         userDao = new UserDaoImpl(dbManager);
+        doBind();
         doUserSaveExample();
+    }
+
+    private void doBind() {
+        dbManager.bindRealtimeData(RegisterFingerprint.COLLECTION, new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String val = dataSnapshot.getValue(String.class);
+                Log.d(TAG, RegisterFingerprint.COLLECTION + " changed! ======== Value: " + val);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.i(TAG, "Bind canceled: " + RegisterFingerprint.COLLECTION);
+            }
+        });
     }
     
     private int getPostfix() {
@@ -57,9 +80,14 @@ public class ExampleManager {
                     u.setId(documentSnapshot.getId());
                     mUser = u;
                     doPrintUser();
+                    updateRegFingerPrint();
                 }
             }
         });
+    }
+
+    private void updateRegFingerPrint() {
+        dbManager.setRealtimeData(RegisterFingerprint.COLLECTION, RegisterFingerprint.FAILED);
     }
     
     private void doPrintUser() {
